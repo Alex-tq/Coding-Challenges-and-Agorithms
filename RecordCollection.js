4f91a0e81ab0ff8898d8e23cf615fe9ac6804fc3
Alexander <alextq3036@gmail.com>
2019-12-25 13:57:49 -0500

    Update RecordCollection.js

--- RecordCollection.js
+++ RecordCollection.js
@@ -9,25 +9,18 @@ Each album has several properties and a unique id number as its key. Not all alb
 Write a function which takes an album's id(like 2548), a property prop(like "artist"or "tracks"), 
 and a value(like "Addicted to Love") to modify the data in this collection.
 
-If propisn't "tracks"and valueisn't empty (""), update or set the valuefor that record album's property.
+If prop isn't "tracks"and value isn't empty (""), update or set the valuefor that record album's property.
 
 Your function must always return the entire collection object.
 
 There are several rules for handling incomplete data:
 
-If propis "tracks"but the album doesn't have a "tracks"property, create an empty array before adding the 
+If prop is "tracks" but the album doesn't have a "tracks"property, create an empty array before adding the 
 new value to the album's corresponding property.
 
-If propis "tracks"and valueisn't empty (""), push the valueonto the end of the album's existing tracksarray.
+If prop is "tracks"and value isn't empty (""), push the value onto the end of the album's existing tracks array.
 
-If valueis empty (""), delete the given propproperty from the album.
-
-Hints
-Use bracket notationwhen accessing object properties with variables.
-
-Push is an array method you can read about on Mozilla Developer Network.
-
-You may refer back to Manipulating Complex Objects Introducing JavaScript Object Notation (JSON) for a refresher.
+If value is empty (""), delete the given property from the album.
 
 */
 
