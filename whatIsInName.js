a770d676eec28841a36416cf9b3a5fbce0da1fca
Alexander T <alextq3036@gmail.com>
2020-01-06 17:47:47 -0500

    added whatIsInName.js

---
+++ whatIsInName.js
@@ -0,0 +1,13 @@
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+  console.log(source.Keys()+'------');
+  arr = source.keys();
+
+  // Only change code above this line
+  return arr;
+}
+let a = 5;
+let pr = whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+console.log('533333333333333333333');
